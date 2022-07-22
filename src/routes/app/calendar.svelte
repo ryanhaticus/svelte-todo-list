8d2d430d00d8bae0c9e7e3856d1bc026d412096f
Ryan Haticus <rhuellen@iastate.edu>
2022-07-22 09:37:01 -0500

    Starting on Sunday

--- src/routes/app/calendar.svelte
+++ src/routes/app/calendar.svelte
@@ -207,13 +207,13 @@
 		<div
 			class="grid grid-cols-7 gap-px border-b border-gray-300 bg-gray-200 text-center text-xs font-semibold leading-6 text-gray-700 lg:flex-none"
 		>
+			<div class="bg-white py-2">S<span class="sr-only sm:not-sr-only">un</span></div>
 			<div class="bg-white py-2">M<span class="sr-only sm:not-sr-only">on</span></div>
 			<div class="bg-white py-2">T<span class="sr-only sm:not-sr-only">ue</span></div>
 			<div class="bg-white py-2">W<span class="sr-only sm:not-sr-only">ed</span></div>
 			<div class="bg-white py-2">T<span class="sr-only sm:not-sr-only">hu</span></div>
 			<div class="bg-white py-2">F<span class="sr-only sm:not-sr-only">ri</span></div>
 			<div class="bg-white py-2">S<span class="sr-only sm:not-sr-only">at</span></div>
-			<div class="bg-white py-2">S<span class="sr-only sm:not-sr-only">un</span></div>
 		</div>
 		<div class="flex bg-gray-200 text-xs leading-6 text-gray-700 lg:flex-auto">
 			<div class="hidden w-full lg:grid lg:grid-cols-7 lg:grid-rows-6 lg:gap-px">
